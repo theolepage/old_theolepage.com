091428adb4053dfccfe3dc11c3f6d71ca5053b72
Theo Lepage <theo.lepage@epita.fr>
2020-02-03 01:20:42 +0100

    Improve resume

--- resources/views/resume/resume.blade.php
+++ resources/views/resume/resume.blade.php
@@ -6,49 +6,56 @@
 
 <div class="resume">
 
-    <div class="resume-name">Theo Lepage</div>
-    <div class="resume-description">Looking for a Autumn internship in Software Engineering starting in September 2020.</div>
+    <div class="resume-header">
+        <div class="resume-head">
+            <div class="resume-name">Theo Lepage</div>
+            <div class="resume-description">Looking for an internship in Software Engineering starting in September 2020.</div>
+        </div>
 
-    <div class="resume-contact">
-        <div class="resume-contact-col">
-            <a class="resume-contact-top" href="https://theolepage.com">
+        <div class="resume-contact">
+            <a class="resume-contact-item" href="https://theolepage.com">
                 <img class="resume-icon" src="/images/icon-website.svg" alt="icon-website">
                 theolepage.com
             </a>
-            <a href="https://www.linkedin.com/in/theolepage/">
-                <img class="resume-icon" src="/images/icon-linkedin.svg" alt="icon-linkedin">
-                Theo Lepage
-            </a>
-        </div>
-        <div class="resume-contact-col">
-            <a class="resume-contact-top" href="mailto:theo@theolepage.com">
-                <img class="resume-icon" src="/images/icon-email.svg" alt="icon-email">
-                theo@theolepage.com
-            </a>
-            <a href="https://github.com/thdoteo">
-                <img class="resume-icon" src="/images/icon-github.svg" alt="icon-github">
-                thdoteo
-            </a>
-        </div>
-        <div class="resume-contact-col"> 
-            <a class="resume-contact-top" href="tel:+33670097066">
+
+            <a class="resume-contact-item" href="tel:+33670097066">
                 <img class="resume-icon" src="/images/icon-phone.svg" alt="icon-phone">
                 +33 6 70 09 70 66
             </a>
-            <a href="https://twitter.com/thdoteo">
-                <img class="resume-icon" src="/images/icon-twitter.svg" alt="icon-twitter">
-                @thdoteo
+
+            <a class="resume-contact-item" href="mailto:theo@theolepage.com">
+                <img class="resume-icon" src="/images/icon-email.svg" alt="icon-email">
+                theo@theolepage.com
             </a>
         </div>
     </div>
     
     <div class="resume-section resume-education">
+        <div class="resume-dot resume-dot_green"></div>
         <div class="resume-title">Education</div>
     
         <div class="resume-item">
             <div class="resume-subtitle">
-                <a href="https://www.epita.fr/en">EPITA – École Pour l'Informatique et les Techniques Avancées</a>
-                (Engineering School)
+                <a href="https://www.lse.epita.fr/">LSE - Laboratoire Sécurité & Système de l'EPITA</a>
+                (Research laboratory)
+            </div>
+            <div class="resume-icons">
+                <div class="resume-location">
+                    <img class="resume-icon" src="/images/icon-map.svg" alt="icon-map">
+                    Paris, France
+                </div>
+                <div class="resume-date">
+                    <img class="resume-icon" src="/images/icon-calendar.svg" alt="icon-calendar">
+                    Started Jan. 2020
+                </div>
+            </div>
+            <div class="resume-text">Student Researcher in machine learning. Currently working on a prototype of a self-driving car.</div>
+        </div>
+
+        <div class="resume-item">
+            <div class="resume-subtitle">
+                <a href="https://www.epita.fr/en">EPITA - École Pour l'Informatique et les Techniques Avancées</a>
+                (Engineering school)
             </div>
             <div class="resume-icons">
                 <div class="resume-location">
@@ -60,12 +67,13 @@
                     Started Sep. 2017
                 </div>
             </div>
-            <div class="resume-text">Master in Computer Science, taught in English as I am part of the international section.</div>
+            <div class="resume-text">Master in Computer Science. First three years taught in English (international section).</div>
         </div>
 
         <div class="resume-item">
             <div class="resume-subtitle">
-                <a href="https://csumb.edu/">CSUMB – California State University of Monterey Bay</a>
+                <a href="https://www.epita.fr/en">CSUMB - California State University, Monterey Bay</a>
+                (Semester abroad)
             </div>
             <div class="resume-icons">
                 <div class="resume-location">
@@ -74,72 +82,130 @@
                 </div>
                 <div class="resume-date">
                     <img class="resume-icon" src="/images/icon-calendar.svg" alt="icon-calendar">
-                    From Jan. 2019 to Jun. 2019
+                    From Jan. to Jun. 2019
                 </div>
             </div>
-            <div class="resume-text">Semester abroad. Worked on a startup project after participating to a Techstars Startup Weekend.</div>
+            <!-- <div class="resume-text">Worked on a startup project after participating to a Techstars Startup Weekend.</div> -->
         </div>
     </div>
 
-    <div class="resume-section resume-experience">
-        <div class="resume-title">Experience</div>
+    <div class="resume-section resume-section_experience">
+        <div class="resume-dot resume-dot_red"></div>
+        <div class="resume-title">Professional experience</div>
 
-        <div class="resume-item">
-            <div class="resume-subtitle">
-                Teaching assistant
-            </div>
-            <div class="resume-icons">
-                <div class="resume-location">
-                    <img class="resume-icon" src="/images/icon-map.svg" alt="icon-map">
-                    Paris, France
+        <div class="resume-experience">
+            <div class="resume-experience-content">
+                <div class="resume-experience-header">
+                    <div class="resume-subtitle">
+                        Teaching Assistant at <a href="https://www.epita.fr/en">EPITA</a>
+                    </div>
+                    <div class="resume-experience-icons">
+                        <div class="resume-location">
+                            <img class="resume-icon" src="/images/icon-map.svg" alt="icon-map">
+                            Paris, France
+                        </div>
+                        <div class="resume-date">
+                            <img class="resume-icon" src="/images/icon-calendar.svg" alt="icon-calendar">
+                            Started Sep. 2019
+                        </div>
+                    </div>
                 </div>
-                <div class="resume-date">
-                    <img class="resume-icon" src="/images/icon-calendar.svg" alt="icon-calendar">
-                    Started Sep. 2019
-                </div>
+                <div class="resume-text">Teaching UNIX concepts and C to second year students through weekly graded practicals.</div>
             </div>
-            <div class="resume-text">Teaching UNIX concepts and C to second year students through weekly graded practicals.</div>
+            <div class="resume-experience-image">
+                <svg class="cv-sc-1hpqbbc-34 itmJrq" viewBox="0 0 645.63498 437.30501"><defs><clipPath id="a"><polygon points="168.501 380.651 645.635 257.905 340.052 28.353 168.501 380.651" style="fill: none;"></polygon></clipPath><linearGradient id="b" x1="24.42662" y1="639.962" x2="25.42662" y2="639.962" gradientTransform="matrix(0, -352.30007, -352.30007, 0, 225865.72601, 8986.15267)" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#004c67"></stop><stop offset="1" stop-color="#00aae7"></stop></linearGradient><clipPath id="c"><polygon points="0 202.971 332.571 437.305 313.678 0 0 202.971" style="fill: none;"></polygon></clipPath><linearGradient id="d" x1="25.46192" y1="639.83387" x2="26.46192" y2="639.83387" gradientTransform="matrix(0, 437.30503, 437.30503, 0, -279636.28422, -11134.62539)" xlink:href="#b"></linearGradient><clipPath id="e"><polygon points="473.781 3.841 605.471 326.317 47.667 326.317 473.781 3.841" style="fill: none;"></polygon></clipPath><linearGradient id="f" x1="24.3736" y1="639.96856" x2="25.3736" y2="639.96856" gradientTransform="matrix(0, -322.47585, -322.47585, 0, 206700.97712, 8186.21334)" xlink:href="#b"></linearGradient></defs><title>logo_epita</title><g style="clip-path: url(&quot;#a&quot;);"><path d="M168.50125,380.65125,645.635,257.905,340.05248,28.35251Z" style="fill: url(&quot;#b&quot;);"></path></g><g style="clip-path: url(&quot;#c&quot;);"><path d="M0,202.97127,332.57125,437.305,313.67751,0Z" style="fill: url(&quot;#d&quot;);"></path></g><g style="clip-path: url(&quot;#e&quot;);"><path d="M473.78123,3.84125l131.69,322.47623H47.66749Z" style="fill: url(&quot;#f&quot;);"></path></g><path d="M108.97313,279.92764h68.42625V257.45389H131.54813V217.32014h45.85125v-22.475H131.54813v-40.1325h45.85125V132.23888H108.97313Zm120.48875-31.605a58.57513,58.57513,0,0,0,58.59376-58.59375,58.57736,58.57736,0,0,0-58.59376-58.495h-24.18V279.92764h22.47375v-31.605Zm-1.70625-94.915h1.70625a36.325,36.325,0,0,1,25.685,62.00626,34.99992,34.99992,0,0,1-25.685,10.635h-1.70625Zm83.47626,126.52h22.47375v-150.19H311.23189Zm42.03375-125.215h36.92249v124.4125h22.475V154.71264h36.92123v-23.2775H353.26562v23.2775Zm173.1,105.24875,7.82749,20.46875h24.07876l-59.49627-154.915-59.59752,154.915h24.07876l7.92625-20.46875h55.18253Zm-27.59-71.7375,18.96252,49.2625H479.81191Z" style="fill: rgb(255, 255, 255);"></path></svg>
+            </div>
         </div>
     </div>
 
     <div class="resume-section resume-projects">
-        <div class="resume-title">Projects</div>
+        <div class="resume-dot resume-dot_blue"></div>
+        <div class="resume-title">Personal projects</div>
 
-        <div class="resume-item">
-            <div class="resume-subtitle">
-                <a href="https://github.com/NoneOfAllOfTheAbove/ocr">An Optical Character Recognition software</a>
+        <div class="resume-cols">
+            <div class="resume-col">
+                <div class="resume-item">
+                    <div class="resume-subtitle">
+                        <a href="https://github.com/NoneOfAllOfTheAbove/ocr">An Optical Character Recognition software</a>
+                    </div>
+                    <div class="resume-text">Led a team during three months with the aim of creating a software, based on a neural network, able to extract text from images.</div>
+                </div>
+
+                <div class="resume-item">
+                    <div class="resume-subtitle">
+                        <a href="https://github.com/AllOfTheAbove/cant-stop-driving">Can't Stop Driving</a>
+                    </div>
+                    <div class="resume-text">Made a 3D multiplayer game built with Unity. The project was chosen by a jury to be presented to new students.</div>
+                </div>
             </div>
-            <div class="resume-text">Led a team during three months with the aim of creating a software, based on a neural network, able to extract text from images.<div>
+
+            <div class="resume-col">
+                <div class="resume-item">
+                    <div class="resume-subtitle">
+                        <a href="https://thdoteo.com">A POSIX shell: 42sh</a>
+                    </div>
+                    <div class="resume-text">Implemented a shell in 4 weeks in C by following the Command Shell Language standard.</div>
+                </div>
+
+                <div class="resume-item">
+                    <div class="resume-subtitle">
+                        <a href="https://thdoteo.com">My website: thdoteo.com</a>
+                    </div>
+                    <div class="resume-text">Created a website with a dashboard to manage and present some of my 30+ open source projects.</div>
+                </div>
+            </div>
         </div>
 
-        <div class="resume-item">
-            <div class="resume-subtitle">
-                <a href="https://github.com/AllOfTheAbove/cant-stop-driving">Can't Stop Driving</a>
-            </div>
-            <div class="resume-text">Made a 3D multiplayer game built with Unity. The project was chosen by a jury to be presented to new students.<div>
-        </div>
-
-        <div class="resume-item">
-            <div class="resume-subtitle">
-                <a href="https://thdoteo.com">thdoteo.com</a>
-            </div>
-            <div class="resume-text">Created a website with a dashboard to manage and present some of my 30+ open source projects.<div>
-        </div>
     </div>
 
     <div class="resume-section resume-interests-skills">
+        <div class="resume-dot resume-dot_orange"></div>
         <div class="resume-title">Interests and skills</div>
 
-        <div class="resume-item">
-            <div class="resume-subtitle">Programming languages / softwares</div>
-            <div class="resume-text">Algorithmics, C, C++, C#, Python, PHP, JavaScript, Java, Git, Linux, Bash, LaTex.</div>
-        </div>
+        <div class="resume-cols">
+            <div class="resume-col">
+                <div class="resume-item">
+                    <div class="resume-subsubtitle">Programming languages / softwares</div>
+                    <div class="resume-label">Algorithmics</div>
+                    <div class="resume-label">C</div>
+                    <div class="resume-label">C++</div>
+                    <div class="resume-label">C#</div>
+                    <div class="resume-label">Java</div>
+                    <div class="resume-label">Python</div>
+                    <div class="resume-label">OCaml / Haskell</div>
+                    <div class="resume-label">PHP</div>
+                    <div class="resume-label">JavaScript</div>
+                </div>
 
-        <div class="resume-item">
-            <div class="resume-subtitle">Passions</div>
-            <div class="resume-text"> - Sailing (sailing instructor).<div>
-            <div class="resume-text"> - Traveling (USA, Mexico, England, Ireland, Italy, Egypt, Morocco, Senegal, Jordan...).</div>
+                <div class="resume-item">
+                    <div class="resume-subsubtitle">Miscellaneous skills</div>
+                    <div class="resume-label">Project management</div>
+                    <div class="resume-label">Git</div>
+                    <div class="resume-label">Linux / Bash</div>
+                    <div class="resume-label">LaTex</div>
+                </div>
+            </div>
+
+            <div class="resume-col">
+                <div class="resume-item">
+                    <div class="resume-subsubtitle">Certificates</div>
+                    <div class="resume-label">TOEIC 865</div>
+                    <div class="resume-label">Driving license</div>
+                    <div class="resume-label">Sailing instructor diploma</div>
+                </div>
+
+                <div class="resume-item">
+                    <div class="resume-subsubtitle">Passions</div>
+                    <div class="resume-label">Sailing and windsurf</div>
+                    <div class="resume-label">Science and artificial intelligence</div>
+                    <div class="resume-label">Traveling (USA, England, Mexico, Senegal, Jordan, ...)</div>
+                </div>
+            </div>
         </div>
+    </div>
+
+    <div class="resume-footer">
+        An interactive and up-to-date version of this document is available at <a href="https://theolepage.com/resume">theolepage.com/resume</a>.
     </div>
 
 </div>
