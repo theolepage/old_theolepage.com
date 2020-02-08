cd24f6c0d28f8b1387634215f5c66701e3bcdf29
Theo Lepage <theo.lepage@epita.fr>
2020-02-08 19:14:09 +0100

    Improve design of internship message box

--- storage/framework/views/d94750ee1dc0b1849a10e1d813daa42cea0a173d.php
+++ storage/framework/views/d94750ee1dc0b1849a10e1d813daa42cea0a173d.php
@@ -4,22 +4,17 @@
 
 <div class="lab wrapper_lab">
 
-<div class="lab-internship">
-        <span class="lab-internship-title">Available for an internship</span>
-        <div class="lab-internship-text">
-            I am currently seeking an internship in Software Engineering starting in September 2020.<br />
-            <a data-turbolinks="false" href="/resume">Learn more by reading my resume</a>
-            <span>(also available in <a data-turbolinks="false" href="/resume/fr">French</a>)</span>
-        </div>
-    </div>
-
     <div class="heading">
         <div class="heading-title">Laboratory</div>
         <div class="heading-subtitle">Hello, my name is Theo and I am a computer science student.</div>
         <div class="heading-subtitle">My "laboratory" aims to present some of my current and past projects.</div>
     </div>
 
-    
+    <div class="lab-internship">
+        I am currently seeking an internship in Software Engineering starting in September 2020.
+        <a data-turbolinks="false" href="/resume">Learn more by reading my resume</a>
+        (also available in <a data-turbolinks="false" href="/resume/fr">French</a>)
+    </div>    
 
     <div class="lab-container">
         <div class="lab-title">Projects</div>
